69a0809f3997c7ff0934240368cdbf55012e0141
Björn <46351249+goanec82@users.noreply.github.com>
2023-09-30 11:34:24 +0200

    Update rpi_first_start.sh

--- rpi_first_start.sh
+++ rpi_first_start.sh
@@ -6,5 +6,6 @@ sudo SKIP_WARNING=1 rpi-update #bios update for rpi
 sudo apt-get remove locales -y #remove lang
 sudo apt-get install locales-all -y #install lang
 LANG="de_DE.UTF-8" LANGUAGE="de_DE.UTF-8" #set lang
+sudo localectl set-locale LANG=de_DE.UTF-8 #set lang
 sudo systemctl set-default multi-user.target #set textmodus
 sudo apt install screen htop fail2ban
