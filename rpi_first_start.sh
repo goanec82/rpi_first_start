22dd6f58d8371638d89332dc2126217675742a82
Björn <46351249+goanec82@users.noreply.github.com>
2023-09-30 10:10:17 +0200

    Update rpi_first_start.sh

--- rpi_first_start.sh
+++ rpi_first_start.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo sed -i '/^#.* de_DE.* /s/^#//' /etc/locale.gen #set de
 sudo update-locale LANG=de_DE.UTF-8 #set de
-sudo rpi-update
+sudo systemctl set-default multi-user.target #set textmodus
+sudo SKIP_WARNING=1 rpi-update #bios update for rpi
 sudo apt-get upgrade -y 
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
